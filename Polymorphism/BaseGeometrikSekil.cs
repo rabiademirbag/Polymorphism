75a6c2c05de3159a45f9d4199925ab8dafe50a2d
rabiademirbag <rddemirbagg@gmail.com>
2025-05-21 20:28:39 +0300

    Proje dosyası ekle.

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        public virtual void AlanHesapla()
+        {
+            Console.WriteLine("Alan: " + Genislik * Yukseklik);;
+        }
+    }
+    public class Kare:BaseGeometrikSekil
+    {
+        public override void AlanHesapla()
+        {
+            Console.WriteLine("Kare alan hesabı:" + Genislik * Yukseklik);
+        }
+    }
+    public class Dikdortgen:BaseGeometrikSekil
+    {
+        public override void AlanHesapla()
+        {
+            Console.WriteLine("Dikdörtgen alan hesabı:" +Genislik*Yukseklik);
+        }
+    }
+    public class DikUcgen:BaseGeometrikSekil
+    {
+        public override void AlanHesapla()
+        {
+            Console.WriteLine("Üçgen alan hesabı:" +Genislik * Yukseklik/ 2);
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using Polymorphism;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        Kare kare = new Kare{Genislik=2, Yukseklik=5};
+        DikUcgen dikUcgen = new DikUcgen { Genislik = 2, Yukseklik = 5 };
+        Dikdortgen dikdortgen = new Dikdortgen { Genislik = 2, Yukseklik = 5 };
+
+        kare.AlanHesapla();
+        dikUcgen.AlanHesapla();
+        dikdortgen.AlanHesapla();
+    } 
+}
